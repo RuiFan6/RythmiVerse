d79132e93f30458b923dd101a713876ce3a89704
Aarsh2001 <ac5973@srmist.edu.in>
2024-03-18 20:07:33 +0000

    networked the scripts

--- Assets/Scripts/DrumstickR.cs
+++ Assets/Scripts/DrumstickR.cs
@@ -1,88 +1,59 @@
 using System.Collections;
 using System.Collections.Generic;
-using Ubiq.Messaging;
 using UnityEngine;
+using Ubiq.Messaging;
 
 public class DrumstickR : MonoBehaviour
 {
-    private Animator animator_R;
-    private NetworkContext context;
+    NetworkContext context;
 
-    // Public variable to manually assign ownership for testing
-    public bool isOwnerForTesting;
+    private DrumKitManager manager;
 
-    private struct DrumstickMessage
-    {
-        public string action;
-        public Vector3 position;
-        public Quaternion rotation;
-    }
+    Vector3 lastPosition;
+    Quaternion lastRotation;
 
     // Start is called before the first frame update
     void Start()
     {
-        animator_R = GetComponent<Animator>();
         context = NetworkScene.Register(this);
-
-        // Set isOwner based on the public variable. This allows manual control for testing.
-        isOwner = isOwnerForTesting;
-
-        Debug.Log($"[DrumstickR] Start - isOwnerForTesting: {isOwnerForTesting}, isOwner: {isOwner}");
+        manager = DrumKitManager.Instance;
     }
 
     // Update is called once per frame
     void Update()
     {
-        // Only allow action initiation if this instance is set as the owner for testing
-        if (animator_R != null && isOwner)
+        // Only the owner should send updates
+        if(manager.isOwner && (lastPosition != transform.localPosition || lastRotation != transform.localRotation))
         {
-            if (Input.GetMouseButtonDown(0))
-            {
-                animator_R.SetTrigger("hit");
-                SendAction("hit");
-                Debug.Log("[DrumstickR] Hit action initiated.");
-            }
+            lastPosition = transform.localPosition;
+            lastRotation = transform.localRotation;
 
-            if (Input.GetKeyDown(KeyCode.Q))
+            context.SendJson(new Message()
             {
-                animator_R.SetTrigger("spin");
-                SendAction("spin");
-                Debug.Log("[DrumstickR] Spin action initiated.");
-            }
+                position = transform.localPosition,
+                rotation = transform.localRotation,
+                // token = token // Include the token in the message
+            });
         }
     }
 
-    private void SendAction(string action)
+    private struct Message
     {
-        // Send action along with current position and rotation over the network
-        DrumstickMessage message = new DrumstickMessage 
-        { 
-            action = action,
-            position = this.transform.position,
-            rotation = this.transform.rotation
-        };
-        context.SendJson(message);
-        Debug.Log($"[DrumstickR] Sending action: {action} with position and rotation.");
+        public Vector3 position;
+        public Quaternion rotation;
+        // public int token; // Token for ownership logic
     }
 
-    public void ProcessMessage(ReferenceCountedSceneGraphMessage m)
+    public void ProcessMessage(ReferenceCountedSceneGraphMessage message)
     {
-        // Process received action along with position and rotation
-        var message = m.FromJson<DrumstickMessage>();
+        var m = message.FromJson<Message>();
 
-        // Set the drumstick's position and rotation to the received values
-        this.transform.position = message.position;
-        this.transform.rotation = message.rotation;
+        // Update the object only if the incoming token is higher
+        transform.localPosition = m.position;
+        transform.localRotation = m.rotation;
 
-        Debug.Log($"[DrumstickR] Received action: {message.action} with position {message.position} and rotation {message.rotation}.");
-    }
-
-    // Optional: For future use if you decide to implement dynamic ownership.
-    public bool isOwner;
-    private void AttemptTakeOwnership()
-    {
-        // Placeholder for future ownership logic
-        isOwner = false;
-        Debug.Log("[DrumstickR] Attempting to take ownership.");
+        // Make sure the logic in Update doesn't trigger as a result of this update
+        lastPosition = transform.localPosition;
+        lastRotation = transform.localRotation; 
     }
 }
